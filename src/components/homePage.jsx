e1aee1eb4cf14c5785c8281c693affb5dc3e01bc
Fabricio Antonio Godoy <FAGodoy@dmenargas.gov.ar>
2024-11-06 17:00:03 -0300

    iniciando proyecto

---
+++ src/components/homePage.jsx
@@ -0,0 +1,109 @@
+import React from 'react'
+
+export const HomePage = () => {
+  return (
+    <div className='contenedorPadreHomePage'>
+      <img src="../../img/pcNegra.jpg" alt="pc" className='imgPcNegra'/>
+      <h2>Nuestros servicios</h2>
+      <div  className='ordenarArticle'>    
+        <div>
+          <div className='divArticle'>
+            <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/laptop.png" alt="laptop"/>
+            <ol>
+            <h3>macOS</h3>
+                        <li>Dianóstico de fallos.</li>
+                        <li>Reparación de Logic Board y Displays.</li>
+                        <li>Atención de equipos mojados.</li>
+                        <li>Cambio de bateria.</li>
+                        <li>Actualización de macOS y optimización de Soft.</li>
+                        <li>Upgrade de macOS equipos vintage.</li>
+                        <li>Instalación de software.</li>
+                        <li>Desbloqueo de iCloud.</li>
+            </ol>
+          </div>
+          <div className='divArticle'>
+            <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/laptop.png" alt="laptop"/>
+            <ol>
+            <h3>iOS</h3>
+                        <li>Dianóstico de fallos.</li>
+                        <li>Reparación de Logic Board y Displays.</li>
+                        <li>Atención de equipos mojados.</li>
+                        <li>Cambio de bateria.</li>
+                        <li>Actualización de macOS y optimización de Soft.</li>
+                        <li>Upgrade de macOS equipos vintage.</li>
+                        <li>Instalación de software.</li>
+                        <li>Desbloqueo de iCloud.</li>
+            </ol>
+          </div>
+                </div>
+          <div className='divArticle'>
+            <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/laptop.png" alt="laptop"/>
+            <ol>
+            <h3>Equipos Windows</h3>
+                        <li>Dianóstico de fallos.</li>
+                        <li>Reparación de Logic Board y Displays.</li>
+                        <li>Atención de equipos mojados.</li>
+                        <li>Cambio de bateria.</li>
+                        <li>Actualización de macOS y optimización de Soft.</li>
+                        <li>Upgrade de macOS equipos vintage.</li>
+                        <li>Instalación de software.</li>
+                        <li>Desbloqueo de iCloud.</li>
+            </ol>
+          </div>
+        </div>
+{/*         <div className='divArticle'>
+      
+          <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/laptop.png" alt="laptop"/>
+          <ol>
+          <h3>Canales de Reparación</h3>
+                      <li>Dianóstico de fallos.</li>
+                      <li>Reparación de Logic Board y Displays.</li>
+                      <li>Atención de equipos mojados.</li>
+                      <li>Cambio de bateria.</li>
+                      <li>Actualización de macOS y optimización de Soft.</li>
+                      <li>Upgrade de macOS equipos vintage.</li>
+                      <li>Instalación de software.</li>
+                      <li>Desbloqueo de iCloud.</li>
+          </ol>
+      
+        </div>
+        <div className='divArticle'>
+      
+          <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/laptop.png" alt="laptop"/>
+          <ol>
+          <h3>Empresas</h3>
+                      <li>Dianóstico de fallos.</li>
+                      <li>Reparación de Logic Board y Displays.</li>
+                      <li>Atención de equipos mojados.</li>
+                      <li>Cambio de bateria.</li>
+                      <li>Actualización de macOS y optimización de Soft.</li>
+                      <li>Upgrade de macOS equipos vintage.</li>
+                      <li>Instalación de software.</li>
+                      <li>Desbloqueo de iCloud.</li>
+          </ol>
+      
+        </div>
+        <div className='divArticle'>
+      
+          <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/laptop.png" alt="laptop"/>
+          <ol>
+          <h3>Mobilesentrix</h3>
+                      <li>Dianóstico de fallos.</li>
+                      <li>Reparación de Logic Board y Displays.</li>
+                      <li>Atención de equipos mojados.</li>
+                      <li>Cambio de bateria.</li>
+                      <li>Actualización de macOS y optimización de Soft.</li>
+                      <li>Upgrade de macOS equipos vintage.</li>
+                      <li>Instalación de software.</li>
+                      <li>Desbloqueo de iCloud.</li>
+          </ol>
+      
+        </div>
+
+
+ */}
+
+
+    </div>
+  )
+}
---
+++ src/components/navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div className='navBar'>
+      <a href="/"><img src="../../img/logoTransparente.svg"fill="currentColor" alt="logoTransparente" className='logoTransparente'/></a>
+
+    </div>
+  )
+}
