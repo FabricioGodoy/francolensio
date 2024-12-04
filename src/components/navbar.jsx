650be1bb9466bc3be6c6214bfd21eb15aec0a119
Fabricio Antonio Godoy <FAGodoy@dmenargas.gov.ar>
2024-12-04 10:55:20 -0300

    cambios iconos color de vuelta

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -18,17 +18,17 @@ export const NavBar = () => {
   
       <a href="https://www.facebook.com/share/sd8ok8dScFDe35aE/?mibextid=LQQJ4d" target="_blank" rel="noopener noreferrer" className="contact-icon">
        <img className='iconosContacto' src="https://img.icons8.com/color/48/facebook-new.png" alt="facebook"></img>
-       <img className='iconosContactoBN'src="https://img.icons8.com/ios/50/facebook-new.png" alt="facebookBN"></img>
+       <img className='iconosContactoBN'src="https://img.icons8.com/ios-glyphs/30/facebook-new.png" alt="facebookBN"></img>
       </a>
     
       <a href="https://www.instagram.com/simpletecnoapp?igsh=cHc0aWkxNWZoYTd1" target="_blank" rel="noopener noreferrer" className="contact-icon">
        <img className='iconosContacto' src="https://img.icons8.com/fluency/48/instagram-new.png" alt="instagram"></img>
-       <img className='iconosContactoBN'src="https://img.icons8.com/ios/50/instagram-new--v1.png" alt="instagramBN"></img>
+       <img className='iconosContactoBN'src="https://img.icons8.com/ios-filled/50/instagram-new--v1.png" alt="instagramBN"></img>
       </a>
   
       <a href="mailto:contacto@simpletecno.com.ar" className="contact-icon">
        <img className='iconosContacto' src="https://img.icons8.com/fluency/48/apple-mail.png" alt="mail"></img>
-       <img className='iconosContactoBN' src="https://img.icons8.com/ios/50/mail.png" alt="mailBN"></img>
+       <img className='iconosContactoBN' src="https://img.icons8.com/ios-filled/50/mail.png" alt="mailBN"></img>
       </a>
   
     </div>
