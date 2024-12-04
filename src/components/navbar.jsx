4f41a073012f5649c5faeeb4ed8c4e53c47cc63c
Fabricio Antonio Godoy <FAGodoy@dmenargas.gov.ar>
2024-12-04 15:38:21 -0300

    iconos

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -17,18 +17,18 @@ export const NavBar = () => {
     <div className="contact-buttons">
   
       <a href="https://www.facebook.com/share/sd8ok8dScFDe35aE/?mibextid=LQQJ4d" target="_blank" rel="noopener noreferrer" className="contact-icon">
-       <img className='iconosContacto' src="https://img.icons8.com/color/48/facebook-new.png" alt="facebook"></img>
+       <img className='iconosContacto' src="https://img.icons8.com/ios-filled/50/FFFFFF/facebook-new.png" alt="facebook"></img>
        <img className='iconosContactoBN'src="https://img.icons8.com/ios-filled/50/FFFFFF/facebook-new.png" alt="facebookBN"></img>
       </a>
     
       <a href="https://www.instagram.com/simpletecnoapp?igsh=cHc0aWkxNWZoYTd1" target="_blank" rel="noopener noreferrer" className="contact-icon">
-       <img className='iconosContacto' src="https://img.icons8.com/fluency/48/instagram-new.png" alt="instagram"></img>
+       <img className='iconosContacto' src="https://img.icons8.com/ios-filled/50/FFFFFF/instagram-new--v1.png" alt="instagram"></img>
        <img className='iconosContactoBN'src="https://img.icons8.com/ios-filled/50/FFFFFF/instagram-new--v1.png" alt="instagramBN"></img>
       </a>
   
       <a href="mailto:contacto@simpletecno.com.ar" className="contact-icon">
-       <img className='iconosContacto' src="https://img.icons8.com/fluency/48/apple-mail.png" alt="mail"></img>
-       <img className='iconosContactoBN correoIconBN' src="https://img.icons8.com/ios-glyphs/30/FFFFFF/secured-letter--v1.png" alt="mailBN"></img>
+       <img className='iconosContacto' src="https://img.icons8.com/ios-glyphs/30/FFFFFF/secured-letter--v1.png" alt="mail"></img>
+       <img className='iconosContactoBN' src="https://img.icons8.com/ios-glyphs/30/FFFFFF/secured-letter--v1.png" alt="mailBN"></img>
       </a>
   
     </div>
